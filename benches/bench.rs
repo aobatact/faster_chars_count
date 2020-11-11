b3d6cafcd0db8988f429f442786a0b0b0ad1285a
aobatact <aobatact144@gmail.com>
2020-11-11 22:51:19 +0900

    add more bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,6 +4,7 @@
 extern crate test;
 use std::ptr;
 use std::mem::forget;
+use faster_char_count::*;
 
 pub fn black_box<T>(dummy: T) -> T{    unsafe {
     let ret = ptr::read_volatile(&dummy);
@@ -13,11 +14,10 @@ pub fn black_box<T>(dummy: T) -> T{    unsafe {
 
 const TEST_STR1 : &str = "錆";
 const TEST_STR2 : &str = "錆,rust;";
-const TEST_STR3 : &str = "錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;";
+const TEST_STR3 : &str = "錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;";
 const TEST_STR4 : &str = "錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;
 錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;";
 const TEST_STR5 : &str = "錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;
-錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;
 錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;
 錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;";
 const TEST_STR6 : &str = "錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;
@@ -36,16 +36,20 @@ const TEST_STR6 : &str = "錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;
 錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;錆,rust;";
 
 
+fn chars_count_my_bench(b: &mut test::Bencher,s: &str) {
+    b.iter(|| black_box(s.chars_count()))
+}
+
 fn chars_count_std_bench(b: &mut test::Bencher,s: &str) {
     b.iter(|| black_box(s.chars().count()))
 }
 
 fn u32_bench(b: &mut test::Bencher,s: &str) {
-    b.iter(|| black_box(faster_char_count::chars_count_u32(s)))
+    b.iter(|| black_box(chars_count_u32(s)))
 }
 
 fn avx2_bench(b: &mut test::Bencher,s: &str) {
-    b.iter(|| black_box(faster_char_count::chars_count_256(s)))
+    b.iter(|| black_box(chars_count_256(s)))
 }
 
 #[bench]
@@ -134,6 +138,125 @@ fn u32_bench_6(b: &mut test::Bencher) {
 fn avx2_bench_6(b: &mut test::Bencher) {
     avx2_bench(b,TEST_STR6)
 }
+
+
+fn u64_bench(b: &mut test::Bencher,s: &str) {
+    b.iter(|| black_box(faster_char_count::chars_count_u64(s)))
+}
+
+#[bench]
+fn u64_bench_1(b: &mut test::Bencher) {
+    u64_bench(b,TEST_STR1)
+}
+#[bench]
+fn u64_bench_2(b: &mut test::Bencher) {
+    u64_bench(b,TEST_STR2)
+}
+#[bench]
+fn u64_bench_3(b: &mut test::Bencher) {
+    u64_bench(b,TEST_STR3)
+}
+#[bench]
+fn u64_bench_4(b: &mut test::Bencher) {
+    u64_bench(b,TEST_STR4)
+}
+#[bench]
+fn u64_bench_5(b: &mut test::Bencher) {
+    u64_bench(b,TEST_STR5)
+}
+#[bench]
+fn u64_bench_6(b: &mut test::Bencher) {
+    u64_bench(b,TEST_STR6)
+}
+
+
+fn usize_bench(b: &mut test::Bencher,s: &str) {
+    b.iter(|| black_box(faster_char_count::chars_count_usize(s)))
+}
+
+#[bench]
+fn usize_bench_1(b: &mut test::Bencher) {
+    usize_bench(b,TEST_STR1)
+}
+#[bench]
+fn usize_bench_2(b: &mut test::Bencher) {
+    usize_bench(b,TEST_STR2)
+}
+#[bench]
+fn usize_bench_3(b: &mut test::Bencher) {
+    usize_bench(b,TEST_STR3)
+}
+#[bench]
+fn usize_bench_4(b: &mut test::Bencher) {
+    usize_bench(b,TEST_STR4)
+}
+#[bench]
+fn usize_bench_5(b: &mut test::Bencher) {
+    usize_bench(b,TEST_STR5)
+}
+#[bench]
+fn usize_bench_6(b: &mut test::Bencher) {
+    usize_bench(b,TEST_STR6)
+}
+
+
+fn u8_bench(b: &mut test::Bencher,s: &str) {
+    b.iter(|| black_box(faster_char_count::chars_count_u8(s)))
+}
+
+#[bench]
+fn u8_bench_1(b: &mut test::Bencher) {
+    u8_bench(b,TEST_STR1)
+}
+#[bench]
+fn u8_bench_2(b: &mut test::Bencher) {
+    u8_bench(b,TEST_STR2)
+}
+#[bench]
+fn u8_bench_3(b: &mut test::Bencher) {
+    u8_bench(b,TEST_STR3)
+}
+#[bench]
+fn u8_bench_4(b: &mut test::Bencher) {
+    u8_bench(b,TEST_STR4)
+}
+#[bench]
+fn u8_bench_5(b: &mut test::Bencher) {
+    u8_bench(b,TEST_STR5)
+}
+#[bench]
+fn u8_bench_6(b: &mut test::Bencher) {
+    u8_bench(b,TEST_STR6)
+}
+
+
+#[bench]
+fn chars_count_my_bench_1(b: &mut test::Bencher) {
+    chars_count_my_bench(b,TEST_STR1)
+}
+#[bench]
+fn chars_count_my_bench_2(b: &mut test::Bencher) {
+    chars_count_my_bench(b,TEST_STR2)
+}
+#[bench]
+fn chars_count_my_bench_3(b: &mut test::Bencher) {
+    chars_count_my_bench(b,TEST_STR3)
+}
+#[bench]
+fn chars_count_my_bench_4(b: &mut test::Bencher) {
+    chars_count_my_bench(b,TEST_STR4)
+}
+#[bench]
+fn chars_count_my_bench_5(b: &mut test::Bencher) {
+    chars_count_my_bench(b,TEST_STR5)
+}
+#[bench]
+fn chars_count_my_bench_6(b: &mut test::Bencher) {
+    chars_count_my_bench(b,TEST_STR6)
+}
+
+
+
 
 
 /*
--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,30 @@ pub trait CharsCount {
 
 impl CharsCount for str{
     fn chars_count(&self) -> usize {
-        chars_count_256(self)
+        let slice : &[u8] = self.as_ref();
+        let (pre,mid_count,suf) = match slice.len() {
+            258 ..= usize::MAX if cfg!(target_arch = "x86_64") && is_x86_feature_detected!("avx2") => {
+                unsafe{
+                    let (pre,mid,suf) = slice.align_to::<__m256i>();
+                    (pre, count_256(mid),suf)
+                }
+            }
+            11 ..= usize::MAX => {
+                unsafe{
+                    let (pre,mid,suf) = slice.align_to::<usize>();
+                    (pre, count_usize(mid) ,suf)
+                }
+            }
+            _ => return count_u8(slice)
+        };
+        count_u8(pre) + count_u8(suf) + mid_count
     }
 }
 
 pub fn chars_count_256(s: &str) -> usize {
     #[cfg(target_arch = "x86_64")]{
-        use std::arch::x86_64::*;
-        if is_x86_feature_detected!("avx2") {
-            let slice : &[u8] = s.as_ref();
+        let slice : &[u8] = s.as_ref();
+        if is_x86_feature_detected!("avx2") {                
             unsafe{
                 let (pre,mid,suf) = slice.align_to::<__m256i>();
                 return count_u8(pre) + count_256(mid) + count_u8(suf)
@@ -26,9 +41,24 @@ pub fn chars_count_256(s: &str) -> usize {
         }
     }
     
-    //println!("no avx2");
-    chars_count_u32(s)
-    //panic!()
+    //fall back
+    chars_count_usize(s)
+}
+
+pub fn chars_count_usize(s: &str) -> usize {
+    let slice : &[u8] = s.as_ref();
+    unsafe{
+        let (pre,mid,suf) = slice.align_to::<usize>();
+        count_u8(pre) + count_usize(mid) + count_u8(suf)
+    }
+}
+
+pub fn chars_count_u64(s: &str) -> usize {
+    let slice : &[u8] = s.as_ref();
+    unsafe{
+        let (pre,mid,suf) = slice.align_to::<u64>();
+        count_u8(pre) + count_64(mid) + count_u8(suf)
+    }
 }
 
 pub fn chars_count_u32(s: &str) -> usize {
@@ -39,14 +69,21 @@ pub fn chars_count_u32(s: &str) -> usize {
     }
 }
 
+
+pub fn chars_count_u8(s: &str) -> usize {
+    let slice : &[u8] = s.as_ref();
+    count_u8(slice)
+}
+
+
 #[inline]
 fn count_u8(slice: &[u8]) -> usize{
     let mut count = 0;
     for c in slice {
-        let ci = *c as i8;
-        //*c & 0xC0 != 0x80
         // 0x00 <= c <= 0x 7f | 0xc0 <= c <= 0xff
-        if ci > -0x41 {
+        //let ci = *c as i8;
+        //if ci > -0x41 { //no diff in bench with below
+        if c & 0xC0 != 0x80{
             count += 1;
         }
     }
@@ -64,11 +101,34 @@ fn count_32(slice: &[u32]) -> usize{
     count
 }
 
+
+#[inline]
+fn count_64(slice: &[u64]) -> usize{
+    let mut count = 0;
+    for c in slice {
+        let f = c | (!c>>1);
+        let n = f & 0x_4040_4040_4040_4040;
+        count += n.count_ones() as usize;
+    }
+    count
+}
+
+
+#[inline]
+fn count_usize(slice: &[usize]) -> usize{
+    let mut count = 0;
+    for c in slice {
+        let f = c | (!c>>1);
+        let n = f & 0x_4040_4040_4040_4040;
+        count += n.count_ones() as usize;
+    }
+    count
+}
+
 #[inline]
 #[target_feature(enable = "avx2")]
 unsafe fn count_256(slice: &[__m256i]) -> usize{
-    if slice.len() == 0 { return 0 }
-    //println!("avx2");
+    //if slice.len() == 0 { return 0 }
     let mut count = 0 as usize;
     let chunks = slice.chunks(255);
     for block in chunks {
