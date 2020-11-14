5fe4a8fd83ceccb4d4b5432923e1204a9fdb520d
aobatact <aobatact144@gmail.com>
2020-11-14 18:31:38 +0900

    fmt

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,27 +2,47 @@
 #![feature(log_syntax)]
 
 extern crate test;
-use std::ptr;
-use std::mem::forget;
-//use rand::{rngs::StdRng,Rng,thread_rng,SeedableRng};
+use criterion::{
+    criterion_group, criterion_main, measurement::Measurement, AxisScale, BenchmarkGroup,
+    BenchmarkId, Criterion, PlotConfiguration,
+};
 use faster_char_count::*;
-use criterion::{criterion_group, criterion_main,BenchmarkId,Criterion,PlotConfiguration,AxisScale,BenchmarkGroup,measurement::Measurement};
+//use rand::{rngs::StdRng, thread_rng, Rng, SeedableRng};
+use std::mem::forget;
+use std::ptr;
 
-pub fn black_box<T>(dummy: T) -> T{    unsafe {
-    let ret = ptr::read_volatile(&dummy);
-    forget(dummy);
-    ret
-}}
+pub fn black_box<T>(dummy: T) -> T {
+    unsafe {
+        let ret = ptr::read_volatile(&dummy);
+        forget(dummy);
+        ret
+    }
+}
 
-fn group_count_bench<'a, M : Measurement>(mut group: BenchmarkGroup<'a, M>, test_strs : impl IntoIterator<Item = &'a (usize,&'a str)> ){
-    for test_str in test_strs{
-        group.bench_with_input(BenchmarkId::new("avx",&test_str.0),  &test_str.1, |b,i| b.iter(|| chars_count_256(i)));
+fn group_count_bench<'a, M: Measurement>(
+    mut group: BenchmarkGroup<'a, M>,
+    test_strs: impl IntoIterator<Item = &'a (usize, &'a str)>,
+) {
+    for test_str in test_strs {
+        group.bench_with_input(BenchmarkId::new("avx", &test_str.0), &test_str.1, |b, i| {
+            b.iter(|| chars_count_256(i))
+        });
         //group.bench_with_input(BenchmarkId::new("usize",&test_str.0),&test_str.1, |b,i| b.iter(|| chars_count_usize(i)));
-        group.bench_with_input(BenchmarkId::new("u8",&test_str.0),   &test_str.1, |b,i| b.iter(|| chars_count_u8(i)));
-        group.bench_with_input(BenchmarkId::new("u32",&test_str.0),  &test_str.1, |b,i| b.iter(|| chars_count_u32(i)));
-        group.bench_with_input(BenchmarkId::new("u64",&test_str.0),  &test_str.1, |b,i| b.iter(|| chars_count_u64(i)));
-        group.bench_with_input(BenchmarkId::new("mix",&test_str.0),  &test_str.1, |b,i| b.iter(|| i.chars_count()));
-        group.bench_with_input(BenchmarkId::new("std",&test_str.0),  &test_str.1, |b,i| b.iter(|| i.chars().count()));
+        group.bench_with_input(BenchmarkId::new("u8", &test_str.0), &test_str.1, |b, i| {
+            b.iter(|| chars_count_u8(i))
+        });
+        group.bench_with_input(BenchmarkId::new("u32", &test_str.0), &test_str.1, |b, i| {
+            b.iter(|| chars_count_u32(i))
+        });
+        group.bench_with_input(BenchmarkId::new("u64", &test_str.0), &test_str.1, |b, i| {
+            b.iter(|| chars_count_u64(i))
+        });
+        group.bench_with_input(BenchmarkId::new("mix", &test_str.0), &test_str.1, |b, i| {
+            b.iter(|| i.chars_count())
+        });
+        group.bench_with_input(BenchmarkId::new("std", &test_str.0), &test_str.1, |b, i| {
+            b.iter(|| i.chars().count())
+        });
     }
 }
 
@@ -30,54 +50,52 @@ fn count_bench_1_small(c: &mut Criterion) {
     let mut test_strs_a = vec![];
     let a = "a";
     let a64 = a.repeat(64);
-    test_strs_a.push((0,""));
-    test_strs_a.push((1,a));
-    test_strs_a.push((64,&a64));
-    for i in [2,4,8,16,32,48].iter(){
-        unsafe{
-            test_strs_a.push((*i,a64.get_unchecked(..a.len()*i)));
+    test_strs_a.push((0, ""));
+    test_strs_a.push((1, a));
+    test_strs_a.push((64, &a64));
+    for i in [2, 4, 8, 16, 32, 48].iter() {
+        unsafe {
+            test_strs_a.push((*i, a64.get_unchecked(..a.len() * i)));
         }
     }
     let mut group = c.benchmark_group("count_bench_1byte_small");
-    group_count_bench(group,test_strs_a.iter());
+    group_count_bench(group, test_strs_a.iter());
 }
 
 fn count_bench_1(c: &mut Criterion) {
     let mut test_strs_a = vec![];
     let a = "a";
     let a10000 = a.repeat(10000);
-    test_strs_a.push((1,a));
-    test_strs_a.push((10000,&a10000));
-    for i in [10,100,1000].iter(){
-        unsafe{
-            test_strs_a.push((*i,a10000.get_unchecked(..a.len()*i)));
+    test_strs_a.push((1, a));
+    test_strs_a.push((10000, &a10000));
+    for i in [10, 100, 1000].iter() {
+        unsafe {
+            test_strs_a.push((*i, a10000.get_unchecked(..a.len() * i)));
         }
     }
     let mut group = c.benchmark_group("count_bench_1byte");
-    let plot_config = PlotConfiguration::default()
-        .summary_scale(AxisScale::Logarithmic);
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
     group.plot_config(plot_config);
-    group_count_bench(group,test_strs_a.iter());
+    group_count_bench(group, test_strs_a.iter());
 }
 
 fn count_bench_3(c: &mut Criterion) {
     let mut test_strs_s = vec![];
     let s = "錆";
     let s10000 = s.repeat(10000);
-    test_strs_s.push((1,s));
-    test_strs_s.push((10000,&s10000));
-    for i in [10,100,1000].iter(){
-        unsafe{
-            test_strs_s.push((*i,s10000.get_unchecked(..s.len()*i)));
+    test_strs_s.push((1, s));
+    test_strs_s.push((10000, &s10000));
+    for i in [10, 100, 1000].iter() {
+        unsafe {
+            test_strs_s.push((*i, s10000.get_unchecked(..s.len() * i)));
         }
     }
     let mut group = c.benchmark_group("count_bench_3byte");
-    let plot_config = PlotConfiguration::default()
-        .summary_scale(AxisScale::Logarithmic);
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
     group.plot_config(plot_config);
-    group_count_bench(group,test_strs_s.iter());
+    group_count_bench(group, test_strs_s.iter());
 }
 
-criterion_group!(benches, count_bench_1,count_bench_3);
+criterion_group!(benches, count_bench_1, count_bench_3);
 criterion_group!(benches_small, count_bench_1_small);
 criterion_main!(benches_small);
--- src/lib.rs
+++ src/lib.rs
@@ -3,87 +3,83 @@
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::*;
 
-
 pub trait CharsCount {
     fn chars_count(&self) -> usize;
 }
 
-impl CharsCount for str{
+impl CharsCount for str {
     fn chars_count(&self) -> usize {
-        let slice : &[u8] = self.as_ref();
-        let (pre,mid_count,suf) = match slice.len() {
-            258 ..= usize::MAX if cfg!(target_arch = "x86_64") && is_x86_feature_detected!("avx2") => {
-                unsafe{
-                    let (pre,mid,suf) = slice.align_to::<__m256i>();
-                    (pre, count_256(mid),suf)
-                }
+        let slice: &[u8] = self.as_ref();
+        let (pre, mid_count, suf) = match slice.len() {
+            258..=usize::MAX
+                if cfg!(target_arch = "x86_64") && is_x86_feature_detected!("avx2") =>
+            unsafe {
+                let (pre, mid, suf) = slice.align_to::<__m256i>();
+                (pre, count_256(mid), suf)
             }
-            11 ..= usize::MAX => {
-                unsafe{
-                    let (pre,mid,suf) = slice.align_to::<usize>();
-                    (pre, count_usize(mid) ,suf)
-                }
-            }
-            _ => return count_u8(slice)
+            11..=usize::MAX => unsafe {
+                let (pre, mid, suf) = slice.align_to::<usize>();
+                (pre, count_usize(mid), suf)
+            },
+            _ => return count_u8(slice),
         };
         count_u8(pre) + count_u8(suf) + mid_count
     }
 }
 
 pub fn chars_count_256(s: &str) -> usize {
-    #[cfg(target_arch = "x86_64")]{
-        let slice : &[u8] = s.as_ref();
-        if is_x86_feature_detected!("avx2") {                
-            unsafe{
-                let (pre,mid,suf) = slice.align_to::<__m256i>();
-                return count_u8(pre) + count_256(mid) + count_u8(suf)
+    #[cfg(target_arch = "x86_64")]
+    {
+        let slice: &[u8] = s.as_ref();
+        if is_x86_feature_detected!("avx2") {
+            unsafe {
+                let (pre, mid, suf) = slice.align_to::<__m256i>();
+                return count_u8(pre) + count_256(mid) + count_u8(suf);
             }
         }
     }
-    
+
     //fall back
     chars_count_usize(s)
 }
 
 pub fn chars_count_usize(s: &str) -> usize {
-    let slice : &[u8] = s.as_ref();
-    unsafe{
-        let (pre,mid,suf) = slice.align_to::<usize>();
+    let slice: &[u8] = s.as_ref();
+    unsafe {
+        let (pre, mid, suf) = slice.align_to::<usize>();
         count_u8(pre) + count_usize(mid) + count_u8(suf)
     }
 }
 
 pub fn chars_count_u64(s: &str) -> usize {
-    let slice : &[u8] = s.as_ref();
-    unsafe{
-        let (pre,mid,suf) = slice.align_to::<u64>();
+    let slice: &[u8] = s.as_ref();
+    unsafe {
+        let (pre, mid, suf) = slice.align_to::<u64>();
         count_u8(pre) + count_64(mid) + count_u8(suf)
     }
 }
 
 pub fn chars_count_u32(s: &str) -> usize {
-    let slice : &[u8] = s.as_ref();
-    unsafe{
-        let (pre,mid,suf) = slice.align_to::<u32>();
+    let slice: &[u8] = s.as_ref();
+    unsafe {
+        let (pre, mid, suf) = slice.align_to::<u32>();
         count_u8(pre) + count_32(mid) + count_u8(suf)
     }
 }
 
-
 pub fn chars_count_u8(s: &str) -> usize {
-    let slice : &[u8] = s.as_ref();
+    let slice: &[u8] = s.as_ref();
     count_u8(slice)
 }
 
-
 #[inline]
-fn count_u8(slice: &[u8]) -> usize{
+fn count_u8(slice: &[u8]) -> usize {
     let mut count = 0;
     for c in slice {
         // 0x00 <= c <= 0x 7f | 0xc0 <= c <= 0xff
         //let ci = *c as i8;
         //if ci > -0x41 { //no diff in bench with below
-        if c & 0xC0 != 0x80{
+        if c & 0xC0 != 0x80 {
             count += 1;
         }
     }
@@ -91,34 +87,32 @@ fn count_u8(slice: &[u8]) -> usize{
 }
 
 #[inline]
-fn count_32(slice: &[u32]) -> usize{
+fn count_32(slice: &[u32]) -> usize {
     let mut count = 0;
     for c in slice {
-        let f = c | (!c>>1);
+        let f = c | (!c >> 1);
         let n = f & 0x_4040_4040;
         count += n.count_ones() as usize;
     }
     count
 }
 
-
 #[inline]
-fn count_64(slice: &[u64]) -> usize{
+fn count_64(slice: &[u64]) -> usize {
     let mut count = 0;
     for c in slice {
-        let f = c | (!c>>1);
+        let f = c | (!c >> 1);
         let n = f & 0x_4040_4040_4040_4040;
         count += n.count_ones() as usize;
     }
     count
 }
 
-
 #[inline]
-fn count_usize(slice: &[usize]) -> usize{
+fn count_usize(slice: &[usize]) -> usize {
     let mut count = 0;
     for c in slice {
-        let f = c | (!c>>1);
+        let f = c | (!c >> 1);
         let n = f & 0x_4040_4040_4040_4040;
         count += n.count_ones() as usize;
     }
@@ -127,14 +121,17 @@ fn count_usize(slice: &[usize]) -> usize{
 
 #[inline]
 #[target_feature(enable = "avx2")]
-unsafe fn count_256(slice: &[__m256i]) -> usize{
+unsafe fn count_256(slice: &[__m256i]) -> usize {
     //if slice.len() == 0 { return 0 }
     let mut count = 0 as usize;
     let chunks = slice.chunks(255);
     for block in chunks {
         let mut sum = _mm256_setzero_si256();
         for s in block {
-            sum = _mm256_sub_epi8(sum, _mm256_cmpgt_epi8(_mm256_load_si256(s), _mm256_set1_epi8(-0x41)));
+            sum = _mm256_sub_epi8(
+                sum,
+                _mm256_cmpgt_epi8(_mm256_load_si256(s), _mm256_set1_epi8(-0x41)),
+            );
         }
         count += avx2_horizontal_sum_epi8(sum);
     }
@@ -143,15 +140,18 @@ unsafe fn count_256(slice: &[__m256i]) -> usize{
 
 #[inline]
 #[target_feature(enable = "avx2")]
-unsafe fn avx2_horizontal_sum_epi8(x :__m256i) -> usize{
+unsafe fn avx2_horizontal_sum_epi8(x: __m256i) -> usize {
     let sumhi = _mm256_unpackhi_epi8(x, _mm256_setzero_si256());
     let sumlo = _mm256_unpacklo_epi8(x, _mm256_setzero_si256());
     let sum16x16 = _mm256_add_epi16(sumhi, sumlo);
     let sum16x8 = _mm256_add_epi16(sum16x16, _mm256_permute2x128_si256(sum16x16, sum16x16, 1));
-    let sum16x4 = _mm256_add_epi16(sum16x8, _mm256_shuffle_epi32(sum16x8, _MM_SHUFFLE(0, 0, 2, 3)));
+    let sum16x4 = _mm256_add_epi16(
+        sum16x8,
+        _mm256_shuffle_epi32(sum16x8, _MM_SHUFFLE(0, 0, 2, 3)),
+    );
     let tmp = _mm256_extract_epi64(sum16x4, 0);
     let mut result = 0;
-    result += (tmp >> 0 ) & 0xffff;
+    result += (tmp >> 0) & 0xffff;
     result += (tmp >> 16) & 0xffff;
     result += (tmp >> 32) & 0xffff;
     result += (tmp >> 48) & 0xffff;
@@ -160,31 +160,31 @@ unsafe fn avx2_horizontal_sum_epi8(x :__m256i) -> usize{
 
 #[cfg(test)]
 mod tests {
-use super::*;
+    use super::*;
 
     #[test]
     fn count_32() {
         let a = "Hello, world!";
-        assert_eq!(a.chars().count(),chars_count_u32(a));
+        assert_eq!(a.chars().count(), chars_count_u32(a));
         let a = "rust=錆";
-        assert_eq!(a.chars().count(),chars_count_u32(a));
+        assert_eq!(a.chars().count(), chars_count_u32(a));
         let a = "rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆";
-        assert_eq!(a.chars().count(),chars_count_u32(a));
+        assert_eq!(a.chars().count(), chars_count_u32(a));
         let a = "rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆
         rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;;rust=錆;rust=錆;rust=錆;;v;rust=錆;rust=錆;;v;rust=錆;rust=錆;v;rust=錆;v;v;v;rust=錆;rust=錆;rust=錆";
-        assert_eq!(a.chars().count(),chars_count_u32(a));
+        assert_eq!(a.chars().count(), chars_count_u32(a));
     }
 
     #[test]
     fn count_avx() {
         let a = "Hello, world!";
-        assert_eq!(a.chars().count(),chars_count_256(a));
+        assert_eq!(a.chars().count(), chars_count_256(a));
         let a = "rust=錆";
-        assert_eq!(a.chars().count(),chars_count_256(a));
+        assert_eq!(a.chars().count(), chars_count_256(a));
         let a = "rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆";
-        assert_eq!(a.chars().count(),chars_count_256(a));
+        assert_eq!(a.chars().count(), chars_count_256(a));
         let a = "rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆
         rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;rust=錆;;rust=錆;rust=錆;rust=錆;;v;rust=錆;rust=錆;;v;rust=錆;rust=錆;v;rust=錆;v;v;v;rust=錆;rust=錆;rust=錆";
-        assert_eq!(a.chars().count(),chars_count_256(a));
+        assert_eq!(a.chars().count(), chars_count_256(a));
     }
 }
